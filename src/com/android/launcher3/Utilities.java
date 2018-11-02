2d56c004e224272878f172e623e83a8451505bd1
ElDainosor <eldainosor@gmail.com>
2018-11-01 21:22:10 -0500

    Set a toggle to show Search bar on first screen
    Instead of hardcoding it on the FeatureFlags, let's make it
    toggleable as any other option would do
    
    Change-Id: I59893308efa357d25bdd3d53e31904e90c4d84ca
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -96,6 +96,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.android.internal.util.bootleggers.BootlegUtils;
+
 /**
  * Various utilities shared amongst the Launcher's classes.
  */
@@ -131,6 +133,8 @@ public final class Utilities {
      */
     public static final int EDGE_NAV_BAR = 1 << 8;
 
+    static final String KEY_SHOW_SEARCHBAR = "pref_show_searchbar";
+
     /**
      * Indicates if the device has a debug build. Should only be used to store additional info or
      * add extra logging and not for changing the app behavior.
@@ -153,6 +157,9 @@ public final class Utilities {
     private static final int CORE_POOL_SIZE = CPU_COUNT + 1;
     private static final int MAXIMUM_POOL_SIZE = CPU_COUNT * 2 + 1;
     private static final int KEEP_ALIVE = 1;
+
+    public static final String SEARCH_PACKAGE = "com.google.android.googlequicksearchbox";
+
     /**
      * An {@link Executor} to be used with async task with no limit on the queue size.
      */
@@ -774,6 +781,13 @@ public final class Utilities {
             return mSize;
         }
     }
+    public static boolean showQSB(Context context) {
+        SharedPreferences prefs = getPrefs(context.getApplicationContext());
+        if (!BootlegUtils.isPackageInstalled(context, SEARCH_PACKAGE)) {
+            return false;
+        }
+        return prefs.getBoolean(KEY_SHOW_SEARCHBAR, true);
+    }
 
     public static void restart(final Context context) {
         new LooperExecutor(LauncherModel.getWorkerLooper()).execute(() -> {
@@ -784,5 +798,4 @@ public final class Utilities {
             android.os.Process.killProcess(android.os.Process.myPid());
         });
     }
-
 }
