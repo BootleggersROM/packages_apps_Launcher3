42c2ae623054cd4420fcf6bec74f5426173b5f46
ElDainosor <eldainosor@gmail.com>
2018-12-20 01:23:38 +0000

    Launcher3: Disable top/hotseat gradient
    The launcher adds some subtle gradient over the statusbar and the hotseat to highlight the statusbar even on pitch black wallpapers.
    For those who got annoyed by this, we decided to make it a toggle to make them disappear.
    Credits to the pixel launcher bois who nitpicked this.

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -119,6 +119,8 @@ public final class Utilities {
     public static final int EDGE_NAV_BAR = 1 << 8;
 
     public static final String KEY_SHOW_SEARCHBAR = "pref_show_searchbar";
+    public static final String SHOW_WORKSPACE_GRADIENT = "pref_show_workspace_grad";
+    public static final String SHOW_HOTSEAT_GRADIENT = "pref_show_hotseat_grad";
 
     /**
      * Indicates if the device has a debug build. Should only be used to store additional info or
@@ -644,6 +646,14 @@ public final class Utilities {
     static boolean hasFeedIntegration(Context context) {
         SharedPreferences prefs = getPrefs(context.getApplicationContext());
         return prefs.getBoolean(SettingsHomescreen.KEY_FEED_INTEGRATION, true);
+    }
+
+    public static boolean showWorkspaceGradient(Context context) {
+        return getPrefs(context).getBoolean(SHOW_WORKSPACE_GRADIENT, true);
+    }
+
+    public static boolean showHotseatGradient(Context context) {
+        return getPrefs(context).getBoolean(SHOW_HOTSEAT_GRADIENT, true);
     }
 
     public static void restart(final Context context) {
