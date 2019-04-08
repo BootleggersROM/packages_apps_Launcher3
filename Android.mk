2f656eb508253c9d7353cdb52faa30985fa096e4
paphonb <me@paphonb.xyz>
2019-04-08 13:00:58 -0400

    Launcher3: Improve WindowToIcon animation
    - Improve the transition animation
    - Add support for predictions view
    - Open source the overriden transition manager and it's
      dependencies with ported kotlin code from Lawnchair
    
    Bootleggers (@eldainosor) edit: Let's clean everything up and un-lunify
    
    Change-Id: I7f92acab5484aff01114f5af0370aedc820e177a

--- Android.mk
+++ Android.mk
@@ -30,18 +30,6 @@ LOCAL_CERTIFICATE := platform
 include $(BUILD_PREBUILT)
 
 #
-# Prebuilt Luna Launcher shared system
-#
-include $(CLEAR_VARS)
-LOCAL_MODULE := libLunaShared
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_SRC_FILES := libs/libLunaShared.jar
-LOCAL_UNINSTALLABLE_MODULE := true
-LOCAL_SDK_VERSION := 27
-include $(BUILD_PREBUILT)
-
-#
 # Build rule for Launcher3 dependencies lib.
 #
 include $(CLEAR_VARS)
@@ -80,9 +68,6 @@ include $(BUILD_STATIC_JAVA_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_USE_AAPT2 := true
 LOCAL_MODULE_TAGS := optional
-
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    libLunaShared
 LOCAL_STATIC_ANDROID_LIBRARIES := Launcher3CommonDepsLib
 LOCAL_SRC_FILES := \
     $(call all-java-files-under, src) \
@@ -115,8 +100,6 @@ include $(BUILD_PACKAGE)
 include $(CLEAR_VARS)
 LOCAL_USE_AAPT2 := true
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    libLunaShared
 LOCAL_STATIC_ANDROID_LIBRARIES := Launcher3CommonDepsLib
 
 LOCAL_SRC_FILES := \
@@ -158,8 +141,7 @@ LOCAL_AAPT2_ONLY := true
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    libSharedSystemUI \
-    libLunaShared
+    libSharedSystemUI
 LOCAL_STATIC_ANDROID_LIBRARIES := Launcher3CommonDepsLib
 
 LOCAL_SRC_FILES := \
@@ -226,8 +208,7 @@ LOCAL_USE_AAPT2 := true
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    libSharedSystemUI \
-    libLunaShared
+    libSharedSystemUI
 LOCAL_STATIC_ANDROID_LIBRARIES := Launcher3CommonDepsLib
 
 LOCAL_SRC_FILES := \
