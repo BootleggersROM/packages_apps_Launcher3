f8dfe29cd9ab8f6aa1b82fb040cd616192c03e48
tribual <tribual@web.de>
2023-01-18 20:21:24 +0100

    Revert "Launcher3: Add support for toggling taskbar"
    This reverts commit 7823f2d2699a5e04a4103b3cb77d1863cedfd6f0.

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -51,8 +51,6 @@ import com.android.launcher3.util.DisplayController;
 import com.android.launcher3.util.DisplayController.Info;
 import com.android.launcher3.util.WindowBounds;
 
-import android.provider.Settings;
-
 import java.io.PrintWriter;
 import java.util.List;
 import java.util.Locale;
@@ -275,9 +273,7 @@ public class DeviceProfile {
         isTablet = info.isTablet(windowBounds);
         isPhone = !isTablet;
         isTwoPanels = isTablet && useTwoPanels;
-        boolean isTaskBarEnabled = Settings.System.getInt(context.getContentResolver(),
-                Settings.System.ENABLE_TASKBAR, isTablet ? 1 : 0) == 1;
-        isTaskbarPresent = isTaskBarEnabled && ApiWrapper.TASKBAR_DRAWN_IN_PROCESS;
+        isTaskbarPresent = isTablet && ApiWrapper.TASKBAR_DRAWN_IN_PROCESS;
 
         // Some more constants.
         context = getContext(context, info, isVerticalBarLayout() || (isTablet && isLandscape)
