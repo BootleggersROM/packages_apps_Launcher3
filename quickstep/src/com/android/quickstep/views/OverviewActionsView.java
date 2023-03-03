fd33e740b6b0b853b5fe71272ae314792a61eaef
minaripenguin <minaripenguin@users.noreply.github.com>
2023-03-03 22:05:45 +0800

    Launcher3: Rework recent apps UI/UX for QPR1
    * introduce floating clear all button
    * add support for double-line overview actions (inspired by oneUI)
    * adjust overview spacing for new layout
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- quickstep/src/com/android/quickstep/views/OverviewActionsView.java
+++ quickstep/src/com/android/quickstep/views/OverviewActionsView.java
@@ -158,7 +158,7 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
         View clearall = findViewById(R.id.action_clear_all);
         clearall.setOnClickListener(this);
         clearall.setVisibility(mClearAll ? VISIBLE : GONE);
-        findViewById(R.id.clear_all_space).setVisibility(mClearAll ? VISIBLE : GONE);
+        findViewById(R.id.clear_all_container).setVisibility(mClearAll ? VISIBLE : GONE);
 
         View lens = findViewById(R.id.action_lens);
         lens.setOnClickListener(this);
@@ -167,6 +167,7 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
 
         mSplitButton = findViewById(R.id.action_split);
         mSplitButton.setOnClickListener(this);
+        
     }
 
     /**
